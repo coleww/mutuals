3eb400389ca68a56120859809c46bb0b2d989e44
Cole Willsea <coleww@gmail.com>
2016-01-13 08:43:18 -0800

    goals

--- index.js
+++ index.js
@@ -1,116 +1,87 @@
 var Twit = require('twit')
 var T = new Twit(require('./config'))
-
 var sentiment = require('sentiment')
 var wf = require('wordfilter')
 var ic = require('iscool')()
 var tipots = require('this-is-probably-ok-to-say')
-var warnings = ['ceo', 'founder', 'entrepeneur', 'christian', 'husband', 'father', 'big data', 'data vis', 'uber', 'google', 'paypal', 'amazon', 'microsoft', 'gamer', 'gate', 'free speech', 'first amendment', 'gun', 'second amendment']
+var warnings = ['startups', 'investing', 'ceo', 'founder', 'entrepeneur', 'christian', 'husband', 'father', 'big data', 'data vis', 'uber', 'google', 'paypal', 'amazon', 'microsoft', 'gamer', 'gate', 'free speech', 'first amendment', 'gun', 'second amendment']
 var botUnameReggie = /\w+\_?\w+\d*/i
 
-
-
-
-// var MongoClient = require('mongodb').MongoClient
-
-// Connection URL
-// var url = 'mongodb://localhost:27017/myproject'; // um hrm grep herokus?
-
-// if a tweet was faved because it was RT'd by someone I follow, OK
-// if a tweet was faved/RTd by someone who followed me recently, OK fine
-// ANY mention from someone i don;t follow is a block
-// ANY adding to a list from someone i don't follow is a block
-// any interaction from people with [husband father christian, CEO, founder, entrepeneur, etc.] is a block
-
-
-// Use connect method to connect to the Server
-// MongoClient.connect(url, function(err, db) {
-//   if(err) {
-//     return console.error('could not connect to postgres', err)
-//   } else {
+require('mongodb').MongoClient.connect(process.env.MONGOLAB_URI, function(err, db) {
+  if(err) {
+    return console.error('could not connect to postgres', err)
+  } else {
     var stream = T.stream('user', {with: 'user', stringify_friend_id: true})
     stream.on('message', function (t) {
       console.log(t)
-      if (t.friends) {
-        // it's the friends preamble! lets update the list why the heck not?
-      } else if (t.event) {
+      if (t.event) {
         switch (t.event) {
-          // Description Event Name  Source  Target  Target Object
           case 'follow':
             if (t.source.screen_name == 'coleseadubs') {
-              // User follows someone  follow  Current user  Followed user Null
+              // i followed someone
+              approve(t.target.id_str, 'user')
             } else {
-              // User is followed  follow  Following user  Current user  Null
+              // someone followed me
+              examineUser(t.source)
             }
             break;
+          case 'block':
+            // i blocked someone
+            deapprove(t.target.id_str, 'user')
+            break;
           case 'unfollow':
-            // User unfollows someone  unfollow  Current user  Followed user Null
+            // i unfollowed someone
+            deapprove(t.target.id_str, 'user')
             break;
           case 'favorite':
             if (t.source.screen_name == 'coleseadubs') {
-              // User likes a Tweet  favorite  Current user  Tweet author  Tweet
+              // i fave someones tweet (dig for the RT op?)
+              approve(t.target.id_str, 'user')
             } else {
-              // User’s Tweet is liked favorite  Liking user Current user  Tweet
+              // someone faves my tweet
+              examineFaver(t.source, t.target_object)
             }
             break;
           case 'quoted_tweet':
-            // User’s Tweet is quoted  quoted_tweet  quoting User  Current User  Tweet
+            // someone quote RTs my tweet
+            examineRT(t.source, t.target_object)
             break;
         }
-
-
-
-
-
-
-
-
-
-
-
-
-      } else if (t.user) {
-        // if (t.user.screen_name == 'coleseadubs') {
-        //   handleMyAction(t, db)
-        // } else {
-        //   handleInteraction(t, db)
-        // }
+      } else if (t.user && t.user.screen_name !== 'coleseadubs') {
+        // i am being RTd or mentioned i guess
+        if (t.retweeted_status) {
+          examineRT(t.user, t.retweeted_status)
+        } else {
+          examineMention(t)
+        }
       } else {
-        console.log('boop', t)
+        console.log('boop idk whatever', t)
       }
     })
-//   }
-// })
-
-
-function handleMyAction (t, db) {
-  // if i fave a tweet by someone, add them to the thing
-  // if i block someone, add them to the foes list and remove them from friends if they were
-
-}
-
-
-
-function handleInteraction (t, db) {
-  switch (t.event) {
-    case 'follow':
-      sdg
-      break;
-    case 'etc':
-      sdg
-      break;
-    case 'etc':
-      sdg
-      break;
   }
+})
+
+function examineUser (user) {
+
 }
 
+function examineMention (tweet) {
+  // check if user is a friend
+  // if not, block'em
+}
+
+
+function examineRT (user, tweet) {
+  // if user is on friends list
+  // the tweet is chill to get faved
+  // otherwise block'em
+}
 
 // for grabbing bios:
 // GET users/lookup
 // Returns fully-hydrated user objects for up to 100 users per request, as specified by comma-separated values passed to the user_id and/or screen_name parameters.
 
-function examineUser (u) {
+function examineFavorite (user, tweet) {
   // check bio for bs words
   // check if i follow them
   // ummm, check if they follow bullshit?
@@ -123,6 +94,12 @@ function examineUser (u) {
   // profile_background_image_url
   // profile_image_url // idk if there are avi patterns
 
+
+// if a tweet was faved because it was RT'd by someone I follow, OK
+// if a tweet was faved/RTd by someone who followed me recently, OK fine
+// ANY mention from someone i don;t follow is a block
+// ANY adding to a list from someone i don't follow is a block
+// any interaction from people with [husband father christian, CEO, founder, entrepeneur, etc.] is a block
 
 
 
@@ -148,6 +125,15 @@ function examineUser (u) {
 
 
 }
+
+function approve (id_str, type) {
+
+}
+
+function deapprove (id_str, type) {
+
+}
+
 
 function checkIfGood (db, data, cb) {
   var collection = db.collection(data.type);
